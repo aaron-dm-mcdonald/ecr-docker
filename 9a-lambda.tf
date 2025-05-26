b59f3594b76030a1849b30328d92323fa536c754
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-05-25 18:33:44 -0700

    add lambda, start iam role

---
+++ 9a-lambda.tf
@@ -0,0 +1,13 @@
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello-world-lambda"
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.main.repository_url}:latest"
+
+  role = aws_iam_role.lambda_exec_role.arn
+
+  # Optional: Timeout defaults to 3 seconds; you can increase up to 900 (15 minutes)
+  timeout = 30
+
+  depends_on = [ aws_instance.docker, aws_instance.docker,null_resource.wait_for_image ]
+
+}
