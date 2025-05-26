82a3f6aae374f77e1a82bd132816c448888ef780
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-05-25 18:30:36 -0700

    update app

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from mangum import Mangum
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Flask inside Lambda container!"
+
+handler = Mangum(app)  # Wrap Flask app for Lambda
