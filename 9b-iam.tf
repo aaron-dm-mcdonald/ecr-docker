b52b332497af7ba31f887b2ed9e9b0b5c6070abf
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-05-26 11:47:32 -0700

    iam changes

--- 9b-iam.tf
+++ 9b-iam.tf
@@ -16,11 +16,3 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Allow API Gateway to invoke the Lambda
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
-}
